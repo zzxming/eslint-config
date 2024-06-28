678e4c5dd70db8cdb4cad4d0a3308a1e2ae3d1d9
zzxming <zzxming77@163.com>
2024-06-28 18:31:47 +0800

    🐞 fix: 修改unicorn规则

--- src/configs/unicorn.ts
+++ src/configs/unicorn.ts
@@ -11,10 +11,9 @@ export const unicorn = (options: UnicornOptions = {}) => {
       },
       rules: {
         ...pluginUnicorn.configs.recommended.rules,
-        'unicorn/error-message': 'error',
         'unicorn/prevent-abbreviations': 'off',
         'unicorn/no-new-array': 'off',
-        'unicorn/no-new-buffer': 'error',
+        'unicorn/no-new-buffer': 'off',
         'unicorn/no-negated-condition': 'off',
         'unicorn/no-array-callback-reference': 'off',
         'unicorn/filename-case': 'off',
@@ -34,6 +33,11 @@ export const unicorn = (options: UnicornOptions = {}) => {
         'unicorn/prefer-dom-node-remove': 'off',
         'unicorn/prefer-code-point': 'off',
         'unicorn/prefer-at': 'off',
+        'unicorn/import-style': 'off',
+        'unicorn/prefer-top-level-await': 'off',
+        'unicorn/prefer-spread': 'off',
+        'unicorn/no-abusive-eslint-disable': 'off',
+        'unicorn/empty-brace-spaces': 'off',
 
         ...overrides,
       },
