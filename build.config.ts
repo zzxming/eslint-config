238f92ff286593c2b0c83502d97dbfa5c692c35e
zzxming <zzxming77@163.com>
2024-07-13 14:33:03 +0800

    feat: cli update vscode setting

--- build.config.ts
+++ build.config.ts
@@ -1,7 +1,13 @@
 import { defineBuildConfig } from 'unbuild';
 
 export default defineBuildConfig({
-  entries: ['src/index'],
+  entries: [
+    'src/index',
+    {
+      input: 'src/cli/',
+      outDir: 'dist/cli',
+    },
+  ],
   clean: true,
   declaration: true,
   rollup: {
---
+++ src/cli/update-vscode-settings.ts
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+import path from 'node:path';
+import fs from 'node:fs';
+import fsp from 'node:fs/promises';
+import consola from 'consola';
+import { vscodeSettings } from './constants';
+
+const updateVscodeSettings = async () => {
+  const cwd = process.cwd();
+
+  const dotVscodePath: string = path.join(cwd, '.vscode');
+  const settingsPath: string = path.join(dotVscodePath, 'settings.json');
+
+  if (!fs.existsSync(dotVscodePath)) {
+    await fsp.mkdir(dotVscodePath, { recursive: true });
+  }
+
+  if (!fs.existsSync(settingsPath)) {
+    consola.success(`Creating .vscode/settings.json ...`);
+    await fsp.writeFile(settingsPath, `{${vscodeSettings}}\n`, 'utf8');
+    consola.success(`Created .vscode/settings.json`);
+  }
+  else {
+    consola.success(`Updating .vscode/settings.json ...`);
+    let settingsContent = await fsp.readFile(settingsPath, 'utf8');
+    settingsContent = settingsContent.trim().replace(/\s*}$/, '');
+    settingsContent += settingsContent.endsWith(',') || settingsContent.endsWith('{') ? '' : ',';
+    settingsContent += `${vscodeSettings}}\n`;
+
+    await fsp.writeFile(settingsPath, settingsContent, 'utf8');
+    consola.success(`Updated .vscode/settings.json`);
+  }
+};
+
+updateVscodeSettings().catch((error) => {
+  consola.error(error);
+  process.exit(1);
+});
